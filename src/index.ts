7ec00245ffc529504abd58a3656db1906030fe6c
joaquindejtiar <joacodejtiar@gmail.com>
2026-06-29 18:48:55 -0300

    entendiendo... :)

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,23 @@ function repetirLetra(letra: string, cantidad: number): string {
   let resultado : string= "";
 
   let vecesRepetido: number = 0
-  
-  while (vecesRepetido < cantidad){
-    resultado= resultado + letra
-    vecesRepetido = vecesRepetido + 1
-  }
-  return resultado;
+  while (vecesRepetido < cantidad) {
+    resultado = resultado + letra;
+    vecesRepetido = vecesRepetido + 1;
+}
+return resultado
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 let resultado: string = repetirLetra (caracter, tamaño);
-let vecesRepetido: number = 1;
+let vecesRepetido: number = 0;
 
 while (vecesRepetido < tamaño) {
   console.log(resultado)
   vecesRepetido = vecesRepetido + 1;
 }
+
 
 
 
